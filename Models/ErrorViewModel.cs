edc1768724f62481b2e0bfed97b816115aa8f4df
Ryan Dines <ryan.dines@gmail.com>
2018-06-25 20:25:49 -0400

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SeniorDevops.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
